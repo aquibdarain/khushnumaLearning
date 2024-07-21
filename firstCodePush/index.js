e48ac393bf838d287b2a45c260eb08fca4ff70ec
Aquib Darain <darainaquib4@gmail.com>
2024-07-21 12:52:38 +0530

    somthing added

--- firstCodePush/index.js
+++ firstCodePush/index.js
@@ -1,3 +1,10 @@
 
 
 console.log(" This is the first learning of GIT & GITHUB");
+
+console.log("we are done with push commits");
+
+// git add . 
+// git commit -m "updated with push commit line or code "
+
+// git push 
